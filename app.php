8e8dc79a1e66e947bc69ddb5d7a2f90f984c55c9
369tushar <91561647+369tushar@users.noreply.github.com>
2023-07-20 16:34:12 +0530

    Update app.php

--- app.php
+++ app.php
@@ -7,6 +7,7 @@ $fbid = base64_decode($_GET['fbid']);
 $doamin = "https://app.newsspad.com";
 $actual_link = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $portions = explode("/",$actual_link);
+$foldername = $portions[2];
 
 
 include("appconfig.php");
