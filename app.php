7a71b8c178ce397c8424de7f8537b7eafeeeec88
369tushar <91561647+369tushar@users.noreply.github.com>
2023-07-20 16:42:13 +0530

    Update app.php

--- app.php
+++ app.php
@@ -9,30 +9,7 @@ $actual_link = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $portions = explode("/",$actual_link);
 $foldername = $portions[2];
 
+$data = file_get_contents('https://app.newsspad.com/apps/'.$foldername.'/app.php?fbid='.$fbid.');
 
+print_r($data);
 ?>
-
-<!DOCTYPE html>
-<html lang="<?php echo $html_lang; ?>">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="apple-touch-icon" sizes="180x180" href=<?php echo $domain;?>/images/favicon.png>
-<link rel="icon" type="image/png" sizes="32x32" href=<?php echo $domain;?>/images/favicon.png>
-<link rel="icon" type="image/png" sizes="16x16" href=<?php echo $domain;?>/images/favicon.png>
-<link rel="shortcut icon" href=<?php echo $domain;?>/images/favicon.png>
-<meta name="theme-color" content="#ffffff">
-<title><?php echo $titleline;?></title>
-<meta property="og:title" content="<?php echo $titleline;?>">
-<meta property="og:site_name" content="<?php echo $author;?>">
-<meta property="og:description" content="<?php echo $descri;?>">
-<meta name="description" content="<?php echo $descri;?>">
-<meta property="og:type" content="website">
-<meta property="og:image" content="<?php echo $domain;?>/apps/<?php echo $foldername;?>/img/<?php echo $fbid; ?>.jpg">
-<meta property="og:image:width" content="800">
-<meta property="og:image:height" content="420">
-<meta property="og:image:type" content="image/jpeg">
-<meta http-equiv="refresh" content="0; url='<?php echo $domain;?>/apps/<?php echo $foldername;?>/?app'" />
-</head>
-
-</html>
